568d52f9c95c8034b762d48adafb29b45c96f548
Runneso <111322016+Runneso@users.noreply.github.com>
2025-01-13 13:22:03 +0300

    Update SegmentTree.cpp

--- C++/data structures/SegmentTree.cpp
+++ C++/data structures/SegmentTree.cpp
@@ -1,82 +1,93 @@
 #include <bits/stdc++.h>
 
-
- 
-#define ll long long int
-#define ld long double
- 
-#define inf LONG_LONG_MAX
-#define MOD 1e9+7
-#define EPS 1e-9
-#define all(x) x.begin(),x.end()
-#define print(x) for(auto element:x) cout <<element << " ";
-#define endl "\n"
-
-
- 
 using namespace std;
 
-//freopen("file.txt","r",stdin);
-//getline(cin,string);
-//num % mod = (num % mod + mod) % mod
-//rand() % (end - start + 1) + start;
+template<class T>
+struct SegmentTree {
+    int64_t n;
+    vector<T> tree;
 
+    function<T(T, T)> action;
+    T null;
 
-struct SegmentTree{
-    int n;
-    vector<int> tree;
+    explicit SegmentTree(vector<T> &seq) {
+        this->n = seq.size();
+        this->tree.assign(this->n * 4, 0);
 
-    SegmentTree(vector<int> &seq){
-        this->n=seq.size();
-        this->tree = vector<int>(this->n*4,0);
-        this->build(seq,0,this->n-1,0);
+        this->action = [&](T a, T b) {
+            return a + b;
+        };
+
+        this->null = T();
+
+        this->build(seq, 0, this->n - 1, 0);
     }
 
-    void build(vector<int> &seq,int left,int right,int index){
-        if (left==right){
-            this->tree[index]=seq[left];
+
+    explicit SegmentTree(vector<T> &seq, T action(T, T), T null) {
+        this->n = seq.size();
+        this->tree.assign(this->n * 4, 0);
+
+        this->action = action;
+        this->null = null;
+
+        this->build(seq, 0, this->n - 1, 0);
+    }
+
+    void build(vector<T> &seq, const int64_t left, const int64_t right, const int64_t index) {
+        if (left == right) {
+            this->tree[index] = seq[left];
             return;
         }
 
-        int mid = (left+right)/2;
+        const int64_t mid = (left + right) / 2;
 
-        this->build(seq,left,mid,2*index+1);
-        this->build(seq,mid+1,right,2*index+2);
+        this->build(seq, left, mid, 2 * index + 1);
+        this->build(seq, mid + 1, right, 2 * index + 2);
 
-        this->tree[index]=this->tree[2*index+1]+this->tree[2*index+2];
+        this->tree[index] = this->action(this->tree[2 * index + 1], this->tree[2 * index + 2]);
     }
 
-    void update(int left,int right,int index,int pos, int val){
-        if (left==right){
-            this->tree[index]=val;
+    void update(const int64_t left, const int64_t right, const int64_t index, const int64_t pos, const int64_t val) {
+        if (left == right) {
+            this->tree[index] = val;
             return;
         }
 
-        int mid = (left+right)/2;
+        const int64_t mid = (left + right) / 2;
 
-        if (pos<=mid){
-            this->update(left,mid,2*index+1,pos,val);
-        }else{
-            this->update(mid+1,right,2*index+2,pos,val);
+        if (pos <= mid) {
+            this->update(left, mid, 2 * index + 1, pos, val);
+        } else {
+            this->update(mid + 1, right, 2 * index + 2, pos, val);
         }
 
-        this->tree[index]=this->tree[2*index+1]+this->tree[2*index+2];
+        this->tree[index] = this->action(this->tree[2 * index + 1], this->tree[2 * index + 2]);
     }
 
-    int sum(int left,int right,int index,int curr_left,int curr_right){
-        if (curr_right<left || curr_left>right){
-            return 0;
+    T sum(const int64_t left, const int64_t right, const int64_t index, const int64_t curr_left,
+          const int64_t curr_right) {
+        if (curr_right < left || curr_left > right) {
+            return this->null;
         }
 
-        if (curr_left<=left && right<=curr_right){
+        if (curr_left <= left && right <= curr_right) {
             return this->tree[index];
         }
 
-        int mid = (left+right)/2;
+        const int64_t mid = (left + right) / 2;
 
-        int left_sum = this->sum(left,mid,2*index+1,curr_left,curr_right);
-        int right_sum = this->sum(mid+1,right,2*index+2,curr_left,curr_right);
+        T left_sum = this->sum(left, mid, 2 * index + 1, curr_left, curr_right);
+        T right_sum = this->sum(mid + 1, right, 2 * index + 2, curr_left, curr_right);
 
-        return left_sum+right_sum;
+        return this->action(left_sum, right_sum);
+    }
+
+    void modify(const int64_t pos, T val) {
+        this->update(0, this->n - 1, 0, pos, val);
+    }
+
+    T query(const int64_t l, const int64_t r) {
+        return this->sum(0, this->n - 1, 0, l, r);
     }
 };
