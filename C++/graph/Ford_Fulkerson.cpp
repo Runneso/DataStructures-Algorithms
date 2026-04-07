b9cc168433b3ed5e7e1d98f204413cc5983edaee
Runneso <111322016+Runneso@users.noreply.github.com>
2026-04-07 21:56:22 +0300

    Update Ford_Fulkerson.cpp

--- C++/graph/Ford_Fulkerson.cpp
+++ C++/graph/Ford_Fulkerson.cpp
@@ -1,3 +1,100 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+
+#define ll long long int
+#define ld long double
+#define cd complex<ld>
+
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template<class T>
+struct ordered_multiset {
+    ordered_set<pair<T, ll> > t;
+    ll timer = 0;
+
+    void insert(const T &x) {
+        t.insert({x, timer++});
+    }
+
+    void extract(const T &x) {
+        auto it = t.lower_bound({x, -1});
+        if (it != t.end() && it->first == x) {
+            t.erase(it);
+        }
+    }
+
+    ll count_less(const T &x) const {
+        return t.order_of_key({x, -1});
+    }
+
+    ll count_less_or_equal(const T &x) const {
+        return t.order_of_key({x, LLONG_MAX});
+    }
+
+    T kth(ll k) const {
+        return t.find_by_order(k)->first;
+    }
+
+    ll size() const {
+        return (ll) t.size();
+    }
+
+    bool empty() const {
+        return t.empty();
+    }
+};
+
+const ld PI = acos(-1);
+const ll MOD = 1'000'000'007LL;
+const ll MOD_ = 1'000'000'009LL;
+const ll BASE = 911382323LL;
+const ll BASE_ = 972663749LL;
+const ld EPS = 1e-9;
+
+#define all(x) (x).begin(), (x).end()
+#define print(x, a) for (auto element : x) cout << element + a << " ";
+#define endl "\n"
+
+#define I_AM_OUTSIDER = true;
+#define RAMAZAN_RAKHMATULLIN_FAN_CLUB = true;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl;
+#else
+#define debug(x)
+#endif
+
+template<class T>
+vector<T> compress(vector<T> v) {
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    return v;
+}
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+ll randint(const ll l, const ll r) {
+    return rng() % (r - l + 1) + l;
+}
+
+ll binpow(const ll a, const ll p, const ll m) {
+    if (p == 0) {
+        return 1;
+    }
+    if (p & 1) {
+        return (binpow(a, p - 1, m) * a) % m;
+    }
+    const ll res = binpow(a, p / 2, m);
+    return (res * res) % m;
+}
+
 struct Ford_Fulkerson {
     struct Edge {
         ll from, to, cap, flow;
@@ -5,6 +102,7 @@ struct Ford_Fulkerson {
 
     ll n, m, s, t;
     ll edge_id = 0;
+    bool computed = false;
     vector<Edge> edges;
     vector<vector<ll> > graph;
 
@@ -29,7 +127,7 @@ struct Ford_Fulkerson {
         }
     }
 
-    ll max_flow() {
+    void compute() {
         vector<bool> visited(n, false);
         function<ll(ll,ll)> dfs = [&](ll node, ll flow) {
             if (node == t) {
@@ -62,10 +160,70 @@ struct Ford_Fulkerson {
             }
         }
 
+        computed = true;
+    }
+
+    ll max_flow() {
+        assert(computed);
+
         ll flow = 0;
         for (ll edge_id: graph[s]) {
             flow += edges[edge_id].flow;
         }
         return flow;
     }
+
+    vector<ll> min_cut() {
+        assert(computed);
+
+        vector<bool> visited(n);
+        vector<ll> cut;
+
+        function<void(ll)> dfs_cut = [&](ll node) {
+            visited[node] = true;
+            for (ll edge_id: graph[node]) {
+                Edge &edge = edges[edge_id];
+                if (!visited[edge.to] && edge.flow < edge.cap) {
+                    dfs_cut(edge.to);
+                }
+            }
+        };
+
+        dfs_cut(s);
+
+        for (ll index = 0; index < 2 * m; index += 2) {
+            Edge &edge = edges[index];
+            if (visited[edge.from] != visited[edge.to]) {
+                cut.push_back(index);
+            }
+        }
+        return cut;
+    }
 };
+
+void solution() {
+    ll n, m;
+    cin >> n >> m;
+
+    Ford_Fulkerson ff(n, m, 0, n - 1);
+
+    for (ll index = 0; index < m; index++) {
+        ll u, v, c;
+        cin >> u >> v >> c;
+        u--, v--;
+        ff.add_edge(u, v, c);
+    }
+    ff.compute();
+    cout << ff.max_flow() << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t = 1;
+    // cin >> t;
+    while (t--) {
+        solution();
+    }
+}
